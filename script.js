982ec2cb1c8c890fd2a15c0c76de2dd809d328c5
emmayly <93347757+emmayly@users.noreply.github.com>
2021-11-26 00:56:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let myDiv = document.querySelector('div');
+
+myDiv.addEventListener('click', () => {
+    myDiv.style.backgroundImage = `url('image/left_s1_1.png')`;
+    document.getElementById("msg1").style.visibility = "hidden";
+});
