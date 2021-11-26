cc3ff910f0b004a8cf87d160dd0a05dc49663a8f
emmayly <93347757+emmayly@users.noreply.github.com>
2021-11-26 01:10:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ let myDiv = document.querySelector('div');
 myDiv.addEventListener('click', () => {
     myDiv.style.backgroundImage = `url('image/left_s1_1.png')`;
     document.getElementById("msg1").style.visibility = "hidden";
+    document.getElementById("link").style.visibility = "hidden";
 });
