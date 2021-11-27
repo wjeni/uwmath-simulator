274e74fb95f74c0b6f0369db86950d4f581a851e
wjeni <jw8wu@uwaterloo.ca>
2021-11-26 22:48:06 -0500

    Updated right view

--- script.js
+++ script.js
@@ -30,21 +30,45 @@ addEvent(
         hide("right_arrow");
         hide("msg1");
         hide("L_subview1")
-        display("back_arrow");
+        display("L_back_arrow");
     }
 );
-
 
 // Try many different ways of coding in attempt to add an event to another element. All have failed
 // The function works if it is put in the html file using eventListener, however it strangely doesn't worke in the js file.
 addEvent(
-    document.getElementById('back_arrow'),
+    document.getElementById('L_back_arrow'),
     'click',
     function(){
         changebg('url(image/left_view.png)');
         display("right_arrow");
         display("msg1");
         display("L_subview1")
-        hide("back_arrow");
+        hide("L_back_arrow");
+    }
+);
+
+// Following functions on right view failed to register?
+addEvent(
+    document.getElementById('R_subview1'),
+    'click',
+    function(){
+        changebg('url(image/right_s1_1.png)');
+        hide("left_arrow");
+        hide("msg1");
+        hide("R_subview1")
+        display("R_back_arrow");
+    }
+);
+
+addEvent(
+    document.getElementById('R_back_arrow'),
+    'click',
+    function(){
+        changebg('url(image/right_view.png)');
+        display("left_arrow");
+        display("msg1");
+        display("R_subview1")
+        hide("R_back_arrow");
     }
 );
